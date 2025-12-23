6864faf5bdce50a48d9820661e29aba424e36bce
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2025-12-23 07:36:48 +0100

    restructuration globale du code, j'ai rajouté des fonctions pour qu'il puisse être le plus efficace possible

--- Epreuve _de_Code/convertisseur/main.py
+++ Epreuve _de_Code/convertisseur/main.py
@@ -1,12 +1,15 @@
 #Mon Code :
 choix = input("Souhaitez vous convertir de pouces vers cm (a) ou de centimètres vers pouces (b) ? ")
 
-if choix == "a" : 
-    valeur = float (input ("Rentrez votre valeur en pouces (a ou b) : "))
-    conversion = round (valeur*2.54, 2)
-    print (f"{valeur} vaut {conversion} centimètres")
+def convertisseur (choix_utilisateur) :
+    if choix_utilisateur == "a" : 
+        valeur = float (input ("Rentrez votre valeur en pouces (a ou b) : "))
+        conversion = round (valeur*2.54, 2)
+        print (f"{valeur} pouces vaut {conversion} centimètres")
 
-if choix == "b":
-    valeur = float (input ("Rentrez votre valeur en centimètres : "))
-    conversion = round (valeur*0.394, 2)
-    print (f"{valeur} vaut {conversion} pouces")   
+    if choix_utilisateur == "b":
+        valeur = float (input ("Rentrez votre valeur en centimètres : "))
+        conversion = round (valeur*0.394, 2)
+        print (f"{valeur} centimètres vaut {conversion} pouces")  
+
+convertisseur (choix) 
