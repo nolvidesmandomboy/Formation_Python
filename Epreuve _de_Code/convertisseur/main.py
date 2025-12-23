0224befafab8a8169584dff4c89a621b8944b817
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2025-12-23 07:49:42 +0100

    Ajout d'une boucle While avec un booléen jeun == True, ainsi que d'un choix supplémentaire permettant de sortir

--- Epreuve _de_Code/convertisseur/main.py
+++ Epreuve _de_Code/convertisseur/main.py
@@ -1,15 +1,22 @@
 #Mon Code :
-choix = input("Souhaitez vous convertir de pouces vers cm (a) ou de centimètres vers pouces (b) ? ")
+print("Bienvenue dans le convertisseur de pouces en centimètres et vice versa !")
+jeu = True
+while jeu == True :
+    choix = input("Souhaitez vous convertir de pouces vers cm (a), de centimètres vers pouces (b) ou sortir (c) ? ")
 
-def convertisseur (choix_utilisateur) :
-    if choix_utilisateur == "a" : 
-        valeur = float (input ("Rentrez votre valeur en pouces (a ou b) : "))
-        conversion = round (valeur*2.54, 2)
-        print (f"{valeur} pouces vaut {conversion} centimètres")
+    def convertisseur (choix_utilisateur) :
+        if choix_utilisateur == "a" : 
+            valeur = float (input ("Rentrez votre valeur en pouces (a ou b) : "))
+            conversion = round (valeur*2.54, 2)
+            print (f"{valeur} pouces vaut {conversion} centimètres")
 
-    if choix_utilisateur == "b":
-        valeur = float (input ("Rentrez votre valeur en centimètres : "))
-        conversion = round (valeur*0.394, 2)
-        print (f"{valeur} centimètres vaut {conversion} pouces")  
+        if choix_utilisateur == "b":
+            valeur = float (input ("Rentrez votre valeur en centimètres : "))
+            conversion = round (valeur*0.394, 2)
+            print (f"{valeur} centimètres vaut {conversion} pouces")  
 
-convertisseur (choix) 
+    if choix == "c" :
+        print ("Au revoir !")
+        jeu = False
+    else :
+        convertisseur (choix)
