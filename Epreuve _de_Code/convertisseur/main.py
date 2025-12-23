88a99cc0ec426698e87b1b74d8a075bb45218e60
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2025-12-23 09:45:58 +0100

    La boucle while a été mis après la fonction (ça n'a aucun intérêt que celle-ci soit dedans) et j'ai rajouté une condition else dans la boucle If pour gerer les erreurs lorsque l'utilisateur choisi s'il veut convertir en pouce ou en centimètres

--- Epreuve _de_Code/convertisseur/main.py
+++ Epreuve _de_Code/convertisseur/main.py
@@ -1,9 +1,7 @@
 #Mon Code :
 print("Bienvenue dans le convertisseur de pouces en centimètres et vice versa !")
-while True :
-    choix = input("Souhaitez vous convertir de pouces vers cm (a), de centimètres vers pouces (b) ou sortir (c) ? ")
 
-    def convertisseur (unite1, unite2, facteur_conversion) : 
+def convertisseur (unite1, unite2, facteur_conversion) : 
         valeur_str = (input (f"Rentrez votre valeur en {unite1} : "))
         try:
             valeur_float = float(valeur_str) # La conversion en float est faite directement ici, pas besoin de convertir la valeur plus tôt sinon on aura une erreur"
@@ -15,6 +13,9 @@ while True :
         conversion = round (valeur_float*facteur_conversion, 2)
         print (f"{valeur_float} {unite1} vaut {conversion} {unite2}")
 
+while True :
+    choix = input("Souhaitez vous convertir de pouces vers cm (a), de centimètres vers pouces (b) ou sortir (c) ? ")
+
     if choix == "a" :
         convertisseur ("pouces", "centimètres", 2.54)
     elif choix == "b" :
@@ -22,3 +23,5 @@ while True :
     elif choix == "c" :
         print ("Au revoir !")
         break
+    else :
+        print ("ERREUR, veuillez choisir une option valide (a, b ou c).")
