eaefb11d6e3de66fa8b3411b6a25a214ada0469b
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2025-12-23 07:01:32 +0100

    rajout du choix à faire dans la demande "rentrez votre valeur"

--- Epreuve _de_Code/convertisseur/main.py
+++ Epreuve _de_Code/convertisseur/main.py
@@ -2,7 +2,7 @@
 choix = input("Souhaitez vous convertir de pouces vers cm (a) ou de centimètres vers pouces (b) ? ")
 
 if choix == "a" : 
-    valeur = float (input ("Rentrez votre valeur en pouces : "))
+    valeur = float (input ("Rentrez votre valeur en pouces (a ou b) : "))
     conversion = valeur*2.54
     print (f"{valeur} vaut {conversion} centimètres")
 
