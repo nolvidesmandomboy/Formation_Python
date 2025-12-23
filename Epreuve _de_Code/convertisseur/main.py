818c64f322b0b62359324d0f97ba5c0e0802bfea
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2025-12-23 08:44:10 +0100

    Gestion des erreurs lorsque l'utilisateur rentre la valeur à convertir avec un "Try/Except" NB: on doit convertir la valeur DANS le try et non avant

--- Epreuve _de_Code/convertisseur/main.py
+++ Epreuve _de_Code/convertisseur/main.py
@@ -4,9 +4,16 @@ while True :
     choix = input("Souhaitez vous convertir de pouces vers cm (a), de centimètres vers pouces (b) ou sortir (c) ? ")
 
     def convertisseur (unite1, unite2, facteur_conversion) : 
-        valeur = float (input (f"Rentrez votre valeur en {unite1} : "))
-        conversion = round (valeur*facteur_conversion, 2)
-        print (f"{valeur} {unite1} vaut {conversion} {unite2}")
+        valeur_str = (input (f"Rentrez votre valeur en {unite1} : "))
+        try:
+            valeur_float = float(valeur_str) # La conversion en float est faite directement ici, pas besoin de convertir la valeur plu tôt sinon on aura une erreur"
+        except ValueError:
+            print("ERREUR, veuillez utilisez une valeur numérique ")
+            print ("(la décimale est un point et non une virgule)")
+            return convertisseur (unite1, unite2, facteur_conversion)
+        
+        conversion = round (valeur_float*facteur_conversion, 2)
+        print (f"{valeur_float} {unite1} vaut {conversion} {unite2}")
 
     if choix == "a" :
         convertisseur ("pouces", "centimètres", 2.54)
