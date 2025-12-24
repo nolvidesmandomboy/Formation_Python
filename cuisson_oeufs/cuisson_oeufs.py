c080f75299aae574e3a60743baf3a075f7aedf92
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2025-12-24 10:13:58 +0100

    création de l'affichage des choix et de la demande à faire à l'utilisateur

--- cuisson_oeufs/cuisson_oeufs.py
+++ cuisson_oeufs/cuisson_oeufs.py
@@ -1,5 +1,13 @@
 import time
 
+print ("Bienvenue dans ce minuteur du temps de cuisson !")
+print ("Que souhaitez-vous cuire ?")
+print ("a - des oeufs à la coque (3 minutes)")
+print ("b - des oeufs mollets (6 minutes)")
+print ("c - des oeufs durs (10 minutes)")
+
+choix = input ("votre choix ? (entre a, b et c) : ")
+
 for i in range (5) : 
     time.sleep(1)
     print (".", end="", flush=True)
@@ -10,3 +18,5 @@ print("fin du programme")
 d = 100
 min = d//60 # division entière (pas de virgules)
 sec = d-min*60
+
+print(f"{min:02d}")
