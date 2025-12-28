461ad9533a1f4794767b8b645a526e1826fd0de7
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2025-12-28 07:14:56 +0100

    Rajout d'une confirmation pour jouer et d'un temps avant de générer la séquence

--- jeu_simon_memo_sequence/main.py
+++ jeu_simon_memo_sequence/main.py
@@ -7,11 +7,16 @@ def clear_screen():
         os.system('clear')
     else:
         os.system('cls')
+
 sequence_aleatoire = str (random.randint (0,9)) + str(random.randint(0,9)) + str(random.randint (0,9)) + str(random.randint (0,9))
 
 print("Bonjour, bienvenue dans ce jeu de mémorisation !")
 time.sleep (1)
-print ("Retenez cette séquence :")
-print(sequence_aleatoire)
-time.sleep (3)
-clear_screen()
+choix_ = input ("Prêt(e) ? (répondre oui ou non) : ")
+
+if choix_ == "oui" :
+    print ("Retenez cette séquence :")
+    time.sleep (1)
+    print(sequence_aleatoire)
+    time.sleep (3)
+    clear_screen()
