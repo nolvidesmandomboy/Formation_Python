289a5e08b992ec74813ffcbdc73f1734bf900de0
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2025-12-28 07:10:16 +0100

    rajout de la fonction permettant de faire disparaître l'écran, et de la durée avant que l'écran disparaît

--- jeu_simon_memo_sequence/main.py
+++ jeu_simon_memo_sequence/main.py
@@ -2,10 +2,16 @@ import random
 import time
 import os
 
+def clear_screen():
+    if(os.name == 'posix'):
+        os.system('clear')
+    else:
+        os.system('cls')
+sequence_aleatoire = str (random.randint (0,9)) + str(random.randint(0,9)) + str(random.randint (0,9)) + str(random.randint (0,9))
 
 print("Bonjour, bienvenue dans ce jeu de mémorisation !")
-
 time.sleep (1)
 print ("Retenez cette séquence :")
-sequence_aleatoire = str (random.randint (0,9)) + str(random.randint(0,9)) + str(random.randint (0,9)) + str(random.randint (0,9))
 print(sequence_aleatoire)
+time.sleep (3)
+clear_screen()
