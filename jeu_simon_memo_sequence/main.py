1f4cead6746e679c3db34794ec469543bbeee22a
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2025-12-28 08:04:26 +0100

    Rajout d'une boucle while pour la confirmation de l'utilisateur de jouer au jeu, ajout du elif au cas ou il dit non

--- jeu_simon_memo_sequence/main.py
+++ jeu_simon_memo_sequence/main.py
@@ -14,31 +14,39 @@ boucle = True
 
 print("Bonjour, bienvenue dans ce jeu de mémorisation !")
 time.sleep (1)
-choix_ = input ("Prêt(e) ? (répondre oui ou non) : ")
 
-if choix_ == "oui" :
-    while boucle == True :
-        print ("Retenez cette séquence :")
-        time.sleep (1)
-        print(sequence_aleatoire)
-        time.sleep (3)
-        clear_screen()
-        reponse = input ("quelle est la séquence de chiffre ? ")
-        if reponse == sequence_aleatoire :
-            score = score + 2
-            print (f"bien joué ! votre score est de {score}, retenez cette sequence")
-            sequence_aleatoire = sequence_aleatoire + str(random.randint(0,9))
-        elif reponse != sequence_aleatoire :
-            print (f"Mauvaise réponse ! la bonne séquencee était : {sequence_aleatoire}")
-            print (f"Votre score final : {score} points")
-            jeu = True
-            while jeu == True :
-                demande = input ("On recommence ? ")
-                if demande == "oui" :
-                    continue
-                elif demande == "non":
-                    print ("Au revoir, à bientôt !")
-                    jeu = False
-                    boucle = False
-                else :
-                    print ("ERREUR, écrivez oui ou non pour répondre")
+while True :
+    choix_ = input ("Prêt(e) ? (répondre oui ou non) : ")
+
+    if choix_ == "oui" :
+        while boucle == True :
+            print ("Retenez cette séquence :")
+            time.sleep (1)
+            print(sequence_aleatoire)
+            time.sleep (3)
+            clear_screen()
+            reponse = input ("quelle est la séquence de chiffre ? ")
+            if reponse == sequence_aleatoire :
+                score = score + 2
+                print (f"bien joué ! votre score est de {score}, retenez cette sequence")
+                sequence_aleatoire = sequence_aleatoire + str(random.randint(0,9))
+            elif reponse != sequence_aleatoire :
+                print (f"Mauvaise réponse ! la bonne séquencee était : {sequence_aleatoire}")
+                print (f"Votre score final : {score} points")
+                jeu = True
+                while jeu == True :
+                    demande = input ("On recommence ? ")
+                    if demande == "oui" :
+                        continue
+                    elif demande == "non":
+                        print ("Au revoir, à bientôt !")
+                        jeu = False
+                        boucle = False
+                    else :
+                        print ("ERREUR, écrivez oui ou non pour répondre")
+    elif choix_ == "non":
+        choix_sortie = input ("souhaitez vous sortir ? ")
+        if choix_sortie == "oui":
+            print ("Au revoir !")
+            break
+        
