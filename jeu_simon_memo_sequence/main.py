9c70fc22e6e28662ea75a00117c8e2a4e42de601
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2025-12-28 06:56:25 +0100

    ajout du nombre aléatoire

--- jeu_simon_memo_sequence/main.py
+++ jeu_simon_memo_sequence/main.py
@@ -4,3 +4,8 @@ import os
 
 
 print("Bonjour, bienvenue dans ce jeu de mémorisation !")
+
+time.sleep (1)
+print ("Retenez cette séquence :")
+sequence_aleatoire = random.randint (0,9)
+print (sequence_aleatoire)
