00884abec3f6693e7b96988b2ea00ef5fae3f8d6
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2025-12-29 11:18:47 +0100

    commentaire sur les exemples précédents et création d'une fonction

--- Module_d_expertise-fonction/main.py
+++ Module_d_expertise-fonction/main.py
@@ -1,9 +1,12 @@
 #Les Fonctions
 
-nom = "Nolvides"
+'''nom = "Nolvides"
 
 print ("je m'appelle " + nom) # <-Ici on concatène la chaîne avec un paramètre (le nom)
 print ("Je m'appelle", nom) # <- ici la fonction print a deux paramètres ("Je m'appelle" et le nom), séparé par la virgule
 
 nom_ = input ("Votre nom : ") # <- retourne la valeur, c'est une fonction bloquante 
-print ("votre nom est", nom_)
+print ("votre nom est", nom_)'''
+
+def afficher_info_personne ():
+    print()
