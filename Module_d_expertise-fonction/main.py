606154be05328b2cc6bec8cabfba7452d8e89295
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2025-12-29 11:09:22 +0100

    rajout de commentaires concernant les exemples

--- Module_d_expertise-fonction/main.py
+++ Module_d_expertise-fonction/main.py
@@ -2,5 +2,5 @@
 
 nom = "Nolvides"
 
-print ("je m'appelle " + nom)
-print ("Je m'appelle", nom)
+print ("je m'appelle " + nom) # <-Ici on concatène la chaîne avec un paramètre (le nom)
+print ("Je m'appelle", nom) # <- ici la fonction print a deux paramètres ("Je m'appelle" et le nom), séparé par la virgule
