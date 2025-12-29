45957af13768c9cd9af434badaae65443db19e3d
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2025-12-29 11:13:34 +0100

    nouvelle exemple avec la fonction input

--- Module_d_expertise-fonction/main.py
+++ Module_d_expertise-fonction/main.py
@@ -4,3 +4,6 @@ nom = "Nolvides"
 
 print ("je m'appelle " + nom) # <-Ici on concatène la chaîne avec un paramètre (le nom)
 print ("Je m'appelle", nom) # <- ici la fonction print a deux paramètres ("Je m'appelle" et le nom), séparé par la virgule
+
+nom_ = input ("Votre nom : ") # <- retourne la valeur, c'est une fonction bloquante 
+print ("votre nom est", nom_)
