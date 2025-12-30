1787d0f55c7d9764e94c3c84cf0a0f90af0d77fc
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2025-12-30 08:51:39 +0100

    réalisation de l'exercice

--- Module_d_expertise-fonction/main.py
+++ Module_d_expertise-fonction/main.py
@@ -9,7 +9,7 @@ nom_ = input ("Votre nom : ") # <- retourne la valeur, c'est une fonction bloqua
 print ("votre nom est", nom_)'''
 
 #Définition de la fonction
-def afficher_info_personne (nom, age):
+'''def afficher_info_personne (nom, age):
     print(f"La personne est {nom}, son age est de {age} ans")
     print(f"son prénom comporte {len(nom)} lettres")
 
@@ -18,7 +18,7 @@ nom1 = input ("Quel est le nom de la personne 1 ?")
 age1 = input ("Quelle est l'âge de la personne 1 ?")
 
 #Appel de la fonction
-afficher_info_personne(nom1,age1)
+afficher_info_personne(nom1,age1)'''
 
 #Return
 ''' -> Sert à sortir d'une fonction et à renvoyer une valeur, ce n'est cependant pas obligatoire
@@ -27,3 +27,11 @@ afficher_info_personne(nom1,age1)
  - On ne peut pas mettre à la fois un return vide ou un return avec une valeur dans une fonction, ça peut créer des bugs ! 
  
  - Les returns ne sont à utiliser que dans une fonction'''
+
+#Exercice 
+
+def recuperer_et_afficher_infos_personne(numero):
+    nom = input (f"Nom de la personne {numero} : ")
+    age = input (f"Age de la personne {numero} : ")
+    print (f"La personne {numero} est {nom}, son age est de {age} ans")
+    print (f"son nom comporte {len(nom)} lettres")
