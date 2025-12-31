779da06193bef2bfc8b213c7ab1cc471ba81027b
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2025-12-31 12:15:44 +0100

    pour que l'erreur marche, transformation du choix du nombre dans la fonction plutot que dans le programme principale et appel de cette fonction

--- Exercice_table_multiplication/Exercice_table_multiplication.py
+++ Exercice_table_multiplication/Exercice_table_multiplication.py
@@ -8,6 +8,7 @@ def afficher_table_de_multiplication (nombre, nb_tab_min=1, nb_tab_max=10):
         nb_tab_min=nb_tab_min
         nb_tab_max=10
     for i in range (int(nb_tab_min),int(nb_tab_max)+1):
+        nombre = int(nombre)
         print (f"{i} x {nombre} = {i*nombre}")
         time.sleep(0.5)
 
@@ -18,15 +19,16 @@ def gestion_des_erreurs (number,nb_min,nb_max):
         return False   
     if nb_max == int :
         return False
+    return True
     
 
 print ("Bienvenue dans cet exercie de table de multiplication")
 time.sleep(1)
 print ("Le but ici est de vous afficher les tables de multiplication du ou des nombres que vous rentrerez")
 time.sleep(2)
-Choix_numero = int(input ("De quel numero souhaitez vous connaître la table de multiplication ? "))
+Choix_numero = (input ("De quel numero souhaitez vous connaître la table de multiplication ? "))
 choix_min = input ("Vous pouvez aussi choisir d'où commence la table et où elle se termine (si vous n'en rentrez pas, la table sera par défaut de 1 à 10), choississez donc une valeur minimale (ou pas) ")
 choix_max = input ("Et une valeur maximale : ")
-
-afficher_table_de_multiplication (Choix_numero,choix_min,choix_max)
+gestion_des_erreurs (Choix_numero,choix_min,choix_max)
+#afficher_table_de_multiplication (Choix_numero,choix_min,choix_max)
 
