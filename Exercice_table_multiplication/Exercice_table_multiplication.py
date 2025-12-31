206023a06262dd68978a16a67488be371029397f
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2025-12-31 11:23:04 +0100

    correction d'erreur

--- Exercice_table_multiplication/Exercice_table_multiplication.py
+++ Exercice_table_multiplication/Exercice_table_multiplication.py
@@ -1,6 +1,6 @@
 print ("Bienvenue dans cet exercie de table de multiplication")
 print ("Le but ici est de vous afficher les tables de multiplication du ou des nombres que vous rentrerez")
 Choix_numero = input ("De quel numero souhaitez vous connaître la table de multiplication ? ")
-choix_min = input ("Vous pouvez aussi choisir d'où commence la table et où elle se termine (si vous n'en rentrez pas, la table sera par défaut de 1 à 10), choississez donc une valeur minimale ")
+choix_min = input ("Vous pouvez aussi choisir d'où commence la table et où elle se termine (si vous n'en rentrez pas, la table sera par défaut de 1 à 10), choississez donc une valeur minimale (ou pas) ")
 choix_max = input ("Et une valeur maximale : ")
 
