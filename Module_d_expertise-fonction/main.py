e7384a3819c989f0275e257d2f8dfc1e971a20e5
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2025-12-31 10:38:53 +0100

    Erreur de saisie

--- Module_d_expertise-fonction/main.py
+++ Module_d_expertise-fonction/main.py
@@ -40,11 +40,13 @@ afficher_info_personne(nom1,age1)'''
 def afficher_info_personnes (numero, nom, age) :
     print (f"La personne {numero} est {nom}, son age est de {age} ans")
     print (f"son nom comporte {len(nom)} lettres")
+
+
     
 
 def recuperer_et_afficher_infos_personne(numero_personne):
-    nom_personne = input (f"Nom de la personne {numero} : ")
-    age_personne = input (f"Age de la personne {numero} : ")
+    nom_personne = input (f"Nom de la personne {numero_personne} : ")
+    age_personne = input (f"Age de la personne {numero_personne} : ")
     afficher_info_personnes (numero_personne,nom_personne,age_personne)
 
 nb_de_personnes = 3
