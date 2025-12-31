e6a5471f0c978ccb09c0ee4af27a09aa4add2461
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2025-12-31 11:17:41 +0100

    infos en plus et demande du numéro à l'utilisateur

--- Exercice_table_multiplication/Exercice_table_multiplication.py
+++ Exercice_table_multiplication/Exercice_table_multiplication.py
@@ -1,1 +1,3 @@
 print ("Bienvenue dans cet exercie de table de multiplication")
+print ("Le but ici est de vous afficher les tables de multiplication du ou des nombres que vous rentrerez")
+Choix_numero = input ("De quel numero souhaitez vous connaître la table de multiplication ?")
