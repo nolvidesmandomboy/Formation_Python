83de0c271b5149158520070d0b0ca4b4804756e6
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2025-12-31 12:37:28 +0100

    création d'une boucle while et d'une possibilité de sortir du programme à la fin

--- Exercice_table_multiplication/Exercice_table_multiplication.py
+++ Exercice_table_multiplication/Exercice_table_multiplication.py
@@ -28,11 +28,20 @@ print ("Bienvenue dans cet exercie de table de multiplication")
 time.sleep(1)
 print ("Le but ici est de vous afficher les tables de multiplication du ou des nombres que vous rentrerez")
 time.sleep(2)
-Choix_numero = (input ("De quel numero souhaitez vous connaître la table de multiplication ? "))
-choix_min = input ("Vous pouvez aussi choisir d'où commence la table et où elle se termine (si vous n'en rentrez pas, la table sera par défaut de 1 à 10), choississez donc une valeur minimale (ou pas) ")
-choix_max = input ("Et une valeur maximale (ou pas) : ")
-if gestion_des_erreurs (Choix_numero,choix_min,choix_max):
-    print ("Erreur, le programme ne peut pas fonctionner vous avez propablement rentré une valeur invalide dans au moins un des paramètres qui vous a été demander de remplir, vous devez écrire des valeurs numériques")
-else:
-    afficher_table_de_multiplication (Choix_numero,choix_min,choix_max)
+
+while True :
+    Choix_numero = (input ("De quel numero souhaitez vous connaître la table de multiplication ? "))
+    choix_min = input ("Vous pouvez aussi choisir d'où commence la table et où elle se termine (si vous n'en rentrez pas, la table sera par défaut de 1 à 10), choississez donc une valeur minimale (ou pas) ")
+    choix_max = input ("Et une valeur maximale (ou pas) : ")
+    if gestion_des_erreurs (Choix_numero,choix_min,choix_max):
+        print ("Erreur, le programme ne peut pas fonctionner vous avez propablement rentré une valeur invalide dans au moins un des paramètres qui vous a été demander de remplir, vous devez écrire des valeurs numériques")
+    else:
+        afficher_table_de_multiplication (Choix_numero,choix_min,choix_max)
+        choix_sortie = input (f"voici la table de multiplication du numéro {Choix_numero}, souhaitez-vous sortir (a) ou recommencer (b) ?")
+        if choix_sortie == "a":
+            break
+        elif choix_sortie == "b":
+            continue
+        else:
+            print("choisissez une réponse entre a et b")
 
