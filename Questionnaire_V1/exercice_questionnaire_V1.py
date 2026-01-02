ef4d896edf9cedd943a16fe1cca794c0dc075087
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-01-02 11:24:42 +0100

    écriture du code pour une première question

--- Questionnaire_V1/exercice_questionnaire_V1.py
+++ Questionnaire_V1/exercice_questionnaire_V1.py
@@ -1,1 +1,14 @@
 print ("Bienvenue dans ce questionnaire :), répondez au question avec la lettre correspondant aux propositions")
+
+print ("Quelle est la capitale de la France ? ")
+
+print ("a - Nairobi \nb - Paris \nc- New York \nd - Malabo")
+
+reponse = input ("Votre réponse : ")
+
+if reponse == "a" : 
+    print ("Bien joué ! réponse correcte")
+elif reponse in ["b","c","d"]:
+    print ("réponse incorrecte")
+else :
+    print ("écrivez une réponse qui se trouve parmi les choix proposés svp")
