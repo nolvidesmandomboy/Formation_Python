20021b8e362b55cf18277b892906e3472caea293
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-01-02 11:45:39 +0100

    Appelation de la fonction

--- Questionnaire_V1/exercice_questionnaire_V1.py
+++ Questionnaire_V1/exercice_questionnaire_V1.py
@@ -13,3 +13,13 @@ def afficher_question (question,choix,reponse_correcte):
         print ("réponse incorrecte")
 
 afficher_question ("Quelle est la capitale de Paris ?",["a - Paris", "b - Nairobi", "c - Copenhague", "d - Madrid"], "a")
+
+afficher_question ("Comment s'appelle le président actuelle de la France",["a - Paul Biya", "b - Theodoro Obiang Nguema Mbasogo", "c - Macron", "d - Trump"], "c")
+
+afficher_question ("Quel manga connu dans le monde entier a été crée par Masashi Kishimoto ?",["a - Berserk", "b - Naruto", "c - My Hero Academia", "d - DBZ"], "a")
+
+afficher_question("Quelle planète est la plus proche du Soleil ?", ["a - Mercure", "b - Vénus", "c - Mars", "d - Jupiter"], "a")
+
+afficher_question("Qui a peint la Joconde ?", ["a - Vincent Van Gogh", "b - Pablo Picasso", "c - Léonard de Vinci", "d - Claude Monet"], "c")
+
+afficher_question("Quel langage est principalement utilisé pour le développement web côté serveur ?", ["a - HTML", "b - CSS", "c - Python", "d - JavaScript"], "c")
