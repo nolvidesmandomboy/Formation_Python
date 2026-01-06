fdc0c1577b81568c9d99a30d12f39d1bec4b9831
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-01-06 20:42:21 +0100

    erreur dans la bonne réponse

--- Questionnaire_V1/exercice_questionnaire_V1.py
+++ Questionnaire_V1/exercice_questionnaire_V1.py
@@ -16,7 +16,7 @@ afficher_question ("Quelle est la capitale de Paris ?",["a - Paris", "b - Nairob
 
 afficher_question ("Comment s'appelle le président actuelle de la France",["a - Paul Biya", "b - Theodoro Obiang Nguema Mbasogo", "c - Macron", "d - Trump"], "c")
 
-afficher_question ("Quel manga connu dans le monde entier a été crée par Masashi Kishimoto ?",["a - Berserk", "b - Naruto", "c - My Hero Academia", "d - DBZ"], "a")
+afficher_question ("Quel manga connu dans le monde entier a été crée par Masashi Kishimoto ?",["a - Berserk", "b - Naruto", "c - My Hero Academia", "d - DBZ"], "b")
 
 afficher_question("Quelle planète est la plus proche du Soleil ?", ["a - Mercure", "b - Vénus", "c - Mars", "d - Jupiter"], "a")
 
