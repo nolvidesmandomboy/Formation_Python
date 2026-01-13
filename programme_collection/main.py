fc1af64baf371c9cb6190916c86df939d6e6091e
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-01-13 22:25:40 +0100

    Test de boucle for avec tuple

--- programme_collection/main.py
+++ programme_collection/main.py
@@ -4,3 +4,6 @@ print("hello word")
 personne = ("Mélanie", "Victor", "Thomas", "Nicolas") #<- c'est un tuple, contrairement aux listes, c'est avec des parenthèses et c'st fixe, on ne peut pas le modifier ni rajouter des éléments c'est immutable
 
 print (personne[3]) # <- on appelle les éléments du tuple et de la liste de la même manière
+
+for i in range (0,len(personne)):
+    print (i)
