62627b1f8cf76558ed6461486d90cff297bf7cfb
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-01-13 22:18:56 +0100

    Ajout d'infos sur les tuples

--- programme_collection/main.py
+++ programme_collection/main.py
@@ -1,1 +1,6 @@
 print("hello word")
+#Les Tuples 
+
+personne = ("Mélanie", "Victor", "Thomas", "Nicolas") #<- c'est un tuple, contrairement aux listes, c'est avec des parenthèses et c'st fixe, on ne peut pas le modifier ni rajouter des éléments c'est immutable
+
+print (personne[3]) # <- on appelle les éléments du tuple et de la liste de la même manière
