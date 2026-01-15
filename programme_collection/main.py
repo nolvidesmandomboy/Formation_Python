196a62edbd7bb861c6f7c34bea7e03127c4bfe12
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-01-15 20:18:00 +0100

    Mise en commentaire de tut pour tester autres choses

--- programme_collection/main.py
+++ programme_collection/main.py
@@ -1,7 +1,7 @@
 print("hello word")
 #Les Tuples 
 
-personne = ("Mélanie", "Victor", "Thomas", "Nicolas") #<- c'est un tuple, contrairement aux listes, c'est avec des parenthèses et c'st fixe, on ne peut pas le modifier ni rajouter des éléments c'est immutable
+'''personne = ("Mélanie", "Victor", "Thomas", "Nicolas") #<- c'est un tuple, contrairement aux listes, c'est avec des parenthèses et c'st fixe, on ne peut pas le modifier ni rajouter des éléments c'est immutable
 
 print (personne[3]) # <- on appelle les éléments du tuple et de la liste de la même manière
 
@@ -15,4 +15,4 @@ personne_ = ["Carlos", "Victoria", "Alison", "Samira"]
 
 del personne_ [2] #<-- Permet de supprimer des éléments d'une liste
 
-print (personne_)
+print (personne_)'''
