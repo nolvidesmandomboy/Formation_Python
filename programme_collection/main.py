35a868b7dffd0372103fd95c033db3591c10fcb4
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-01-15 20:57:00 +0100

    Commentaire des prints pour un nouveau test

--- programme_collection/main.py
+++ programme_collection/main.py
@@ -36,8 +36,8 @@ def obtenir_infos ():
 
 infos = obtenir_infos()
 
-print (f"Prénom : {str(infos[0])}")
-print (f"Age : {str(infos[1])} ans")
-print (f"Activité : {str(infos[2])}")
-print (f"Taille : {str(infos[3])} m")
+#print (f"Prénom : {str(infos[0])}")
+#print (f"Age : {str(infos[1])} ans")
+#print (f"Activité : {str(infos[2])}")
+#print (f"Taille : {str(infos[3])} m")
 
