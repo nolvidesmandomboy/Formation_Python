7d28845a60a4bb20440e0386086e4eda8a86e5eb
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-01-17 11:42:05 +0100

    Gestion d'erreur au cas où le tuple de la liste de pizza serait vide

--- Projet_Pizza_V1/main.py
+++ Projet_Pizza_V1/main.py
@@ -1,10 +1,18 @@
 def afficher_les_pizzas (liste_de_pizza):
     print(f"----------------Liste des pizzas---------------({len(liste_de_pizza)})")
+    if liste_de_pizza == ():
+        print ("AUCUNE PIZZA")
+        return
+
     for pizza in liste_de_pizza :
         print (pizza)
-    print (f"Première pizza : {liste_de_pizza[1]} ")
+    print (f"Première pizza : {liste_de_pizza[0]} ")
     print (f"Derinère pizza : {liste_de_pizza[-1]} ")
 
 pizza = ("Margherita", "Végétarienne", "4 fromages","Hawai")
+vide = ()
 
 afficher_les_pizzas (pizza)
+afficher_les_pizzas(vide)
+
+
