b5740d9e6d29b30f8f75a4f694304d177b6e2587
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-01-17 12:25:23 +0100

    rajout de la fonction sort et des paramètres dans celle-ci

--- Projet_Pizza_V1/main.py
+++ Projet_Pizza_V1/main.py
@@ -1,6 +1,9 @@
 def pizza_existe (collection,rajout):
     if rajout.lower() in collection : #<- le lower ici permet de mettre tout la chaîne de caractère en miniscule et du coup de quand même détecter si un élément est dans la liste même si il est en majuscule
         return True
+    
+def tri_personnalisee (e):
+    return len(e)
 
 def ajouter_pizza_utilisateur (collection):
     rajout = input ("Pizza à rajouter : ")
@@ -12,6 +15,8 @@ def ajouter_pizza_utilisateur (collection):
 
 def afficher_les_pizzas (liste_de_pizza):
     print(f"----------------Liste des pizzas---------------({len(liste_de_pizza)})")
+
+    '''liste_de_pizza.sort(reverse=True,key=tri_personnalisee) # <- on peut mettre des paramètres dans la fonction sort, parmi ceux-ci y a le reverse qui permet d'inverser les valeurs et key qui permet de personnaliser les tri'''
     if liste_de_pizza == ():
         print ("AUCUNE PIZZA")
         return
