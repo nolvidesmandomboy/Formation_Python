1743502390a9f1368d33e7b823eec3012ccdc4b7
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-01-17 11:50:29 +0100

    Transformation du tuple pizza en liste et création de la fonction permettant de rajouter une pizza à celle-ci puis appel de la fonction

--- Projet_Pizza_V1/main.py
+++ Projet_Pizza_V1/main.py
@@ -1,3 +1,9 @@
+def ajouter_pizza_utilisateur (collection):
+    rajout = input ("Pizza à rajouter : ")
+    collection.append(rajout)
+    return collection
+
+
 def afficher_les_pizzas (liste_de_pizza):
     print(f"----------------Liste des pizzas---------------({len(liste_de_pizza)})")
     if liste_de_pizza == ():
@@ -9,10 +15,10 @@ def afficher_les_pizzas (liste_de_pizza):
     print (f"Première pizza : {liste_de_pizza[0]} ")
     print (f"Derinère pizza : {liste_de_pizza[-1]} ")
 
-pizza = ("Margherita", "Végétarienne", "4 fromages","Hawai")
+pizza = ["Margherita", "Végétarienne", "4 fromages","Hawai"]
 vide = ()
 
+pizza = ajouter_pizza_utilisateur (pizza)
 afficher_les_pizzas (pizza)
-afficher_les_pizzas(vide)
 
 
