c17c238f482b02f4bdeb6a92a91e64a8f7efbedc
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-01-17 11:31:54 +0100

    changement d'emplacement de la fonction

--- Projet_Pizza_V1/main.py
+++ Projet_Pizza_V1/main.py
@@ -1,4 +1,5 @@
-pizza = ("margherita", "végétarienne", "4 fromages","hawai")
-
 def afficher_les_pizzas (collection):
     print("----------------Liste des pizzas---------------")
+
+pizza = ("margherita", "végétarienne", "4 fromages","hawai")
+
