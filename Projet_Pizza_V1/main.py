fb988481780ff7f21c6eb5212fb25ac0b0ee3d88
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-01-17 11:39:29 +0100

    Affichage de la première et de la dernière pizza

--- Projet_Pizza_V1/main.py
+++ Projet_Pizza_V1/main.py
@@ -1,9 +1,9 @@
-#Exercice 1
-
 def afficher_les_pizzas (liste_de_pizza):
     print(f"----------------Liste des pizzas---------------({len(liste_de_pizza)})")
     for pizza in liste_de_pizza :
         print (pizza)
+    print (f"Première pizza : {liste_de_pizza[1]} ")
+    print (f"Derinère pizza : {liste_de_pizza[-1]} ")
 
 pizza = ("Margherita", "Végétarienne", "4 fromages","Hawai")
 
