dbe69f52fb1098f965411f211c906c3f1dc64d95
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-01-18 13:17:03 +0100

    changement d'une valeur du dictionnaire et affichage de celui-ci

--- programme_collection/programme_dictionnaire/main.py
+++ programme_collection/programme_dictionnaire/main.py
@@ -3,3 +3,7 @@ print("Hello word")
 personne = {'nom' : 'Mélanie', "age" : 19, "Taille" : 1.60} 
 
 print(personne)
+
+personne["nom"] = "Claire" #<- on peut directement remplacer une valeur d'un dictionnaire
+
+print (personne)
