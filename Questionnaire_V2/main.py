2f6deb6159b373568e72254b0c30a5d4f98a512d
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-01-22 18:29:34 +0100

    écriture de tuples de question ou l'on mettre les différents éléments des questions qui seront entrés comme paramètre de la fonction

--- Questionnaire_V2/main.py
+++ Questionnaire_V2/main.py
@@ -17,6 +17,43 @@ def afficher_question (question,choix,reponse_correcte):
 
 score = 0
 
+question1 = (
+    "Quelle est la capitale de Paris ?",
+    ("a - Paris", "b - Nairobi", "c - Copenhague", "d - Madrid"),
+    "Paris"
+)
+
+question2 = (
+    "Comment s'appelle le président actuel de la France ?",
+    ("a - Paul Biya", "b - Theodoro Obiang Nguema Mbasogo", "c - Macron", "d - Trump"),
+    "Macron"
+)
+
+question3 = (
+    "Quel manga connu dans le monde entier a été créé par Masashi Kishimoto ?",
+    ("a - Berserk", "b - Naruto", "c - My Hero Academia", "d - DBZ"),
+    "Naruto"
+)
+
+question4 = (
+    "Quelle planète est la plus proche du Soleil ?",
+    ("a - Mercure", "b - Vénus", "c - Mars", "d - Jupiter"),
+    "Mercure"
+)
+
+question5 = (
+    "Qui a peint la Joconde ?",
+    ("a - Vincent Van Gogh", "b - Pablo Picasso", "c - Léonard de Vinci", "d - Claude Monet"),
+    "Léonard de Vinci"
+)
+
+question6 = (
+    "Quel langage est principalement utilisé pour le développement web côté serveur ?",
+    ("a - HTML", "b - CSS", "c - Python", "d - JavaScript"),
+    "Python"
+)
+
+
 afficher_question ("Quelle est la capitale de Paris ?",["a - Paris", "b - Nairobi", "c - Copenhague", "d - Madrid"], "a")
 
 afficher_question ("Comment s'appelle le président actuelle de la France",["a - Paul Biya", "b - Theodoro Obiang Nguema Mbasogo", "c - Macron", "d - Trump"], "c")
