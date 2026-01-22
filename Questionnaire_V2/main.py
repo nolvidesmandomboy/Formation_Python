dd2b819bbc8455b9119349c7d36c57e70bfbe4d9
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-01-22 18:41:31 +0100

    optimisation du code et test de celui-ci

--- Questionnaire_V2/main.py
+++ Questionnaire_V2/main.py
@@ -1,15 +1,17 @@
 print ("Bienvenue dans ce questionnaire :), répondez au question avec la lettre correspondant aux propositions\n")
 
-def afficher_question (question,choix,reponse_correcte):
+def afficher_question (question):
     global score #permet d'integrer la variable du programme principale dans la fonction sans la rentrer en parametre 
 
-    print (question)
+    titre_question =question[0]
+    choix_reponses=question[1]
+    reponse_correcte=question[3]
 
-    print (f"{choix[0]}\n{choix[1]}\n{choix[2]}\n{choix[3]}\n")
+    print (titre_question)
 
-    reponse = input ("Votre réponse : ")
+    reponse_utilisateur = input ("Votre réponse : ")
 
-    if reponse == reponse_correcte : 
+    if reponse_utilisateur == reponse_correcte : 
         score = score + 1
         print ("Bien joué ! réponse correcte\n")
     else:
@@ -54,16 +56,6 @@ question6 = (
 )
 
 
-afficher_question ("Quelle est la capitale de Paris ?",["a - Paris", "b - Nairobi", "c - Copenhague", "d - Madrid"], "a")
-
-afficher_question ("Comment s'appelle le président actuelle de la France",["a - Paul Biya", "b - Theodoro Obiang Nguema Mbasogo", "c - Macron", "d - Trump"], "c")
-
-afficher_question ("Quel manga connu dans le monde entier a été crée par Masashi Kishimoto ?",["a - Berserk", "b - Naruto", "c - My Hero Academia", "d - DBZ"], "b")
-
-afficher_question("Quelle planète est la plus proche du Soleil ?", ["a - Mercure", "b - Vénus", "c - Mars", "d - Jupiter"], "a")
-
-afficher_question("Qui a peint la Joconde ?", ["a - Vincent Van Gogh", "b - Pablo Picasso", "c - Léonard de Vinci", "d - Claude Monet"], "c")
-
-afficher_question("Quel langage est principalement utilisé pour le développement web côté serveur ?", ["a - HTML", "b - CSS", "c - Python", "d - JavaScript"], "c")
+afficher_question (question1)
 
 print (f"votre score est de {score}")
