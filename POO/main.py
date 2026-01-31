e3d4446a886927ff633f0e366892cc66817b0ded
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-01-31 20:55:36 +0100

    Ajout de commentaires

--- POO/main.py
+++ POO/main.py
@@ -1,6 +1,11 @@
-def afficher_infos_personne (nom,age):
+#Programmation impérative
+
+'''def afficher_infos_personne (nom,age):
     print (f"La personne s'appelle {nom}, elle a {age} ans")
 
 def demander_nom ():
     nom = input ("Quelle est votre nom ? ")
-    return nom
+    return nom'''
+
+#Programmation orientée objet
+
