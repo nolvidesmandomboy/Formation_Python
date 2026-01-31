5f3432a25b95f9cede395fea2a9641c55b9fedfa
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-01-31 20:41:45 +0100

    définition de fonctions pour la programmation impérative

--- POO/main.py
+++ POO/main.py
@@ -1,1 +1,6 @@
-print('Hello Word')
+def afficher_infos_personne (nom,age):
+    print (f"La personne s'appelle {nom}, elle a {age} ans")
+
+def demander_nom ():
+    nom = input ("Quelle est votre nom ? ")
+    return nom
