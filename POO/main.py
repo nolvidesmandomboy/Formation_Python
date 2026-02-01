9ac276f14ae90f92f28627c81c9d9d527cd8d15b
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-02-01 20:26:29 +0100

    commentaire de l'exercice mise en situation deux et copier coller de l'exercice mise en situation 2

--- POO/main.py
+++ POO/main.py
@@ -56,7 +56,7 @@ personne3.sepresenter()
 # genre
 #   False : Femme
 #   True  : Homme
-class Personne:
+'''class Personne:
     def __init__(self, nom: str, age: int, genre: bool):
         self.nom = nom   # crée une variable d'instance : nom
         self.age = age
@@ -90,4 +90,29 @@ personne1 = Personne("Jean", 25, True)
 personne1.SePresenter()
 
 personne2 = Personne("Emilie", 17, False)
-personne2.SePresenter()
+personne2.SePresenter()'''
+
+# POO EXERCICE DE MISE EN SITUATION 2
+class Personne:
+    def __init__(self, nom: str):
+        self.nom = nom   # crée une variable d'instance : nom
+        print("Constructeur personne " + self.nom)
+
+    def SePresenter(self, age: int):
+        # Bonjour, je m'appelle Jean, j'ai 30 ans
+        # Je suis majeur
+        print("Bonjour, je m'appelle " + self.nom + ", j'ai " + str(age) + " ans")
+        if self.EstMajeur():
+            print("Je suis majeur")
+        else:
+            print("Je suis mineur")
+        print()
+
+    def EstMajeur(self):
+        return age >= 18
+
+personne1 = Personne("Jean")
+personne1.SePresenter(25)
+
+personne1 = Personne("Emilie")
+personne1.SePresenter(17)
