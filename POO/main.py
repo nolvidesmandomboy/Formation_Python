9aecc009c824b6c13eab5f42e5cd70810bb3bd0d
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-02-01 20:41:17 +0100

    commentair du code de mise en situation 2 et rajout du code de mise en situation 3

--- POO/main.py
+++ POO/main.py
@@ -93,7 +93,7 @@ personne2 = Personne("Emilie", 17, False)
 personne2.SePresenter()'''
 
 # POO EXERCICE DE MISE EN SITUATION 2
-class Personne:
+'''class Personne:
     def __init__(self, nom: str, age: int):
         self.nom = nom   # crée une variable d'instance : nom
         self.age = age 
@@ -113,4 +113,33 @@ personne1 = Personne("Jean", 25)
 personne1.SePresenter()
 
 personne2 = Personne("Emilie", 17)
-personne2.SePresenter()
+personne2.SePresenter()'''
+
+# POO EXERCICE DE MISE EN SITUATION 3
+
+# ---
+class Chat:
+    def __int__(self):
+        self.nom = "inconnu"
+
+    def SePresenter(self, nom_facultatif=""):
+        self.nom = nom_facultatif
+        print("Bonjour, je suis un chat et je m'appelle " + self.nom)
+
+# ---
+class Personne:
+    def __int__(self, nom: str):
+        self.nom = nom
+
+    def SePresenter(self):
+        print("Bonjour, je suis une personne et je m'appelle " + self.nom)
+
+# ---
+chat1 = Chat()
+chat1.SePresenter()  # Bonjour, je suis un chat et je m'appelle inconnu
+
+chat2 = Chat("Garfield")
+chat2.SePresenter()  # Bonjour, je suis un chat et je m'appelle Garfield
+
+personne = Personne("Jean")
+Personne.SePresenter()  # Bonjour, je suis une personne et je m'appelle Jean
