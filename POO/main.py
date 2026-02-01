ccb4202fc1e80ab9bf661f4c25c74ef74eaaf990
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-02-01 08:13:02 +0100

    Commentaire du code précédent et rajout en copier collé du code de mise en situation

--- POO/main.py
+++ POO/main.py
@@ -11,7 +11,7 @@ def demander_nom ():
 
 #Définition des classes 
 
-class Personne:
+'''class Personne:
     def __init__(self, nom="", age=0):
         self.nom = nom
         self.age = age 
@@ -50,4 +50,44 @@ personne3.sepresenter()
 #personne1.nom = "toto" #<- on peut altérer la valeur d'une valeur de la classe en dehors du code
 #personne1.sepresenter()
 
-#print ("Estmajeur2 : ", personne2.estmajeur())
+#print ("Estmajeur2 : ", personne2.estmajeur())'''
+
+# EXERCICE DE MISE EN SITUATION 1
+# genre
+#   False : Femme
+#   True  : Homme
+class Personne:
+    def __init__(self, nom: str, age: int, genre: bool):
+        self.nom = nom   # crée une variable d'instance : nom
+        self.age = age
+        self.genre = genre
+        print("Constructeur personne " + self.nom)
+
+    def SePresenter(self):
+        # Bonjour, je m'appelle Jean, j'ai 30 ans
+        # Je suis majeur
+        if self.genre:
+            print("Bonjour, je m'appelle " + self.nom + ", j'ai " + str(self.age) + " ans")
+            print("Genre : Masculin")
+            if self.EstMajeur():
+                print("Je suis majeur")
+            else:
+                print("Je suis mineur")
+            print()
+        else:
+            print("Bonjour, je m'appelle " + self.nom + ", j'ai " + str(self.age) + " ans")
+            print("Genre : Feminin")
+            if self.EstMajeur():
+                print("Je suis majeure")
+            else:
+                print("Je suis mineure")
+            print()
+
+    def EstMajeur(self):
+        return self.age >= 18
+
+personne1 = Personne("Jean", 25)
+personne1.SePresenter()
+
+personne2 = Personne("Emilie", 17)
+personne2.SePresenter()
