b0ac8538be0c70011fc5111838bf67e41b68ba41
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-02-08 20:26:40 +0100

    Rajout de la tomate pour faire le prchaine exercice et résolution de l'exercice précédent (afficher que les pizza végétarienne puis nin végétariennes uniquement)

--- Projet_Pizza_V2/main.py
+++ Projet_Pizza_V2/main.py
@@ -1,26 +1,33 @@
 #Projet Pizza V2 
 
 class Pizza:
-    def __init__(self,nom="",prix=.0,ingredients=()):
+    def __init__(self,nom="",prix=.0,ingredients=(),not_vegetarienne=True):
         self.nom= nom
         self.prix=prix
         self.ingredients = ingredients
+        self.not_vegetarienne = not_vegetarienne
 
     def afficher_infos (self):
-        print(f"PIZZA {self.nom} : {self.prix}€ ")
+        infos_nom_prix = f"PIZZA {self.nom} : {self.prix}€"
+        if self.not_vegetarienne :
+            print (infos_nom_prix) 
+        else :
+            print (f"{infos_nom_prix} - végétarienne")
+
         print(f"ingrédients : {", ".join(self.ingredients)}")
         print()
 
 
-pizza1 = Pizza ("Reine", 9.2, ("jambon", "champignons", "mozzarella"))
+pizza1 = Pizza ("Reine", 9.2, ("jambon", "champignons", "mozzarella","tomate"))
 
 pizza2 = Pizza ("Orientale", 11.0, ("merguez", "poivrons", "oignons", "mozzarella"))
 
-pizza3 = Pizza ("Végétarienne", 9.5, ("courgettes", "aubergines", "poivrons", "mozzarella"))
+pizza3 = Pizza ("Végétarienne", 9.5, ("courgettes", "aubergines", "poivrons", "mozzarella"),False)
 
-pizza4 = Pizza ("Hawaïenne", 9.8, ("jambon", "ananas", "mozzarella"))
+pizza4 = Pizza ("Hawaïenne", 9.8, ("jambon", "ananas", "mozzarella","tomate"))
 
 pizza = (pizza1,pizza2,pizza3,pizza4)
 
 for pizzas in pizza :
-    pizzas.afficher_infos()
+    if pizzas.not_vegetarienne :
+        pizzas.afficher_infos()
