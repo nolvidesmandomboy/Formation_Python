5a718cc33df5693b79f6f33fa7405315480062aa
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-02-08 20:01:14 +0100

    résolution du premier exercice : création de 4 pizzas, ajout de ceux-ci dans un tuple et affichage de leurs caractéristiques grâce à une boucle

--- Projet_Pizza_V2/main.py
+++ Projet_Pizza_V2/main.py
@@ -9,3 +9,18 @@ class Pizza:
     def afficher_infos (self):
         print(f"PIZZA {self.nom} : {self.prix}€ ")
         print(f"ingrédients : {", ".join(self.ingredients)}")
+        print()
+
+
+pizza1 = Pizza ("Reine", 9.2, ("jambon", "champignons", "mozzarella"))
+
+pizza2 = Pizza ("Orientale", 10.0, ("merguez", "poivrons", "oignons", "mozzarella"))
+
+pizza3 = Pizza ("Végétarienne", 9.5, ("courgettes", "aubergines", "poivrons", "mozzarella"))
+
+pizza4 = Pizza ("Hawaïenne", 9.8, ("jambon", "ananas", "mozzarella"))
+
+pizza = (pizza1,pizza2,pizza3,pizza4)
+
+for pizzas in pizza :
+    pizzas.afficher_infos()
