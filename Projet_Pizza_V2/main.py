bfd0fd548566f6710d23557c2653e8e87627243a
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-02-08 19:49:59 +0100

    création de la class Pizza

--- Projet_Pizza_V2/main.py
+++ Projet_Pizza_V2/main.py
@@ -1,1 +1,10 @@
-print("Hello word")
+#Projet Pizza V2 
+
+class Pizza:
+    def __init__(self,nom="",prix=.0,ingredients=()):
+        self.nom= nom
+        self.prix=prix
+        self.ingredients = ingredients
+
+    def afficher_infos (self):
+        print(f"PIZZA {self.nom} : {self.prix}€ ")
