08c81b768ea9c15a070ec1acf3c65b5b8f28d835
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-02-14 11:03:15 +0100

    cours sur les méthodes d'instance, de classe et statiques

--- POO/main.py
+++ POO/main.py
@@ -273,7 +273,7 @@ for l in e :
 
 #Copie d'objets et __str__
 
-class Personne:
+'''class Personne:
     def __init__(self, nom, age):
         self.nom = nom
         self.age= age
@@ -298,4 +298,33 @@ print (personne2)
 print (personne1 == personne2)
 print (personne1 is personne2)
 
-print (personne1.__dict__ == personne2.__dict__)
+print (personne1.__dict__ == personne2.__dict__)'''
+
+# Méthodes d’instance, de classe et statiques
+
+'''class Personne:
+    TYPE = "Humain"
+    def __init__(self, nom):
+        self.nom = nom
+
+    # Méthode d'instance
+    def se_presenter(self):
+        print(f"Je m'appelle {self.formater_chaine(self.nom)} - " + self.TYPE)
+
+    # premier charactère en majuscule puis minuscules
+    # méthode statique
+    @staticmethod
+    def formater_chaine(a):
+        return a[0].upper() + a[1:].lower()
+
+    @classmethod
+    def methode_de_classe(cls):
+        print("Méthode de classe : " + cls.TYPE)
+
+
+personne1 = Personne("jean")
+personne1.se_presenter()
+
+print(Personne.formater_chaine("toTo"))
+
+Personne.methode_de_classe()'''
