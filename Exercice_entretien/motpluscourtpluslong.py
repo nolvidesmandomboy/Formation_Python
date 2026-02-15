de549e77acc5ac533647510fd0d6d7404a93ceae
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-02-15 21:49:01 +0100

    résolution du mot le plus long et du mot le plus court, implémentation du code dans la fonction et test de celle-ci

--- Exercice_entretien/motpluscourtpluslong.py
+++ Exercice_entretien/motpluscourtpluslong.py
@@ -1,4 +1,18 @@
 s = "un chasseur sachant chasser sait chasser sans son chien"
-
 def get_min_and_max_words (sentense):
-    pass
+    sentense = sentense.split()
+    motlepluslong = sentense[0]
+    motlepluscourt = sentense[0]
+    listtriee = [len(i) for i in sentense]
+    nb_max = max(listtriee)
+    nb_min = print (min(listtriee))
+    for w in sentense :
+        if len(w) == nb_max:
+            motlepluslong = w
+        elif len(w) == nb_min:
+            motlepluscourt = w
+    print ("mot le plus long : " + motlepluslong)
+    print ("mot le plus court : " + motlepluscourt)
+    return motlepluslong, motlepluscourt
+
+get_min_and_max_words(s)
