a2166251606777caec66bc1834d1e69ba3dea164
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-02-15 20:57:52 +0100

    définition de la fonction qui permet de mettre les lettres à l'envers

--- Exercice_entretien/reverse.py
+++ Exercice_entretien/reverse.py
@@ -1,1 +1,9 @@
-s = "Bonjour TOTO"
+s = "Bonjour toto"
+def reverse (variable):
+    tab = []
+    for i in range (1,len(variable)+1):
+        tab.append(variable[-i])
+        yes = "".join(tab)
+    print(yes)
+
+reverse (s)
