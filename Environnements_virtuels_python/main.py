a5929648a5caa2db322d4ce4dcd276436800fa9f
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-02-25 07:17:30 +0100

    commentaires sur les étapes à suivre pour activer un environnement virtuel

--- Environnements_virtuels_python/main.py
+++ Environnements_virtuels_python/main.py
@@ -1,1 +1,13 @@
 print("Hello Word !")
+
+'''
+Pour faire un environnement virtuel faire toutes ces étapes : 
+
+- Aller sur le terminal et taper cd + chemin du projet
+- On tape "python -m venv venv" <- le deuxième venv corresponds au nom qu'on souhaite utiliser pour notre environnement virtuel
+- Un nouveau dossier "Venv" est crée
+- on tape .\venv\scripts\activate et avec ça on active l'environnement virtuel
+- Si on veut sortir de l'environnement virtuel on tape .\venv\scripts\deactivate
+- Ensuite on ouvre VS Code, et quand on nous propose d'activer l'environnement virtuel on clique sur toujour autoriser
+
+'''
