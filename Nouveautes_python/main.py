002807abd367d74fbc552495a3f770030c54df4d
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-02-28 10:08:40 +0100

    utilisation de la fonction de concaténation de dictionnaire avec le "|"

--- Nouveautes_python/main.py
+++ Nouveautes_python/main.py
@@ -6,3 +6,5 @@ dict2 = {"Emilie":(35,"danseuse"),"Jenifer":(18,"étudiante en psychologie crimi
 
 dict1.update(dict2) #<- la fonction update ici permet d'agreger le contenu d'un dictionnaire à un jour qui est mis à jour
 print(dict1)
+repectoire_complet = dict1 | dict2 #<- permet de concatener les deux dictionnaires directement 
+print (repectoire_complet)
