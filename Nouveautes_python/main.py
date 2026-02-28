e11d8f5f0275d745ce919787f3bfc5d8d4752008
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-02-28 10:01:19 +0100

    création de deux dictionnaires

--- Nouveautes_python/main.py
+++ Nouveautes_python/main.py
@@ -1,1 +1,5 @@
 #Nouveauté de Python
+#Union de dictionnaire
+
+dict1 = {"Jean":(20,"dévéloppeur"),"Mark":(45,"Architecte")}
+dict2 = {"Emilie":(35,"danseuse"),"Jenifer":(18,"étudiante en psychologie criminelle")}
