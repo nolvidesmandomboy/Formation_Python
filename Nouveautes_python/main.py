b171297312d35f6f8db48459c65ba385b759d52f
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-02-28 10:03:28 +0100

    utilisation de la fonction update pour les dictionnaires

--- Nouveautes_python/main.py
+++ Nouveautes_python/main.py
@@ -3,3 +3,6 @@
 
 dict1 = {"Jean":(20,"dévéloppeur"),"Mark":(45,"Architecte")}
 dict2 = {"Emilie":(35,"danseuse"),"Jenifer":(18,"étudiante en psychologie criminelle")}
+
+dict1.update(dict2) #<- la fonction update ici permet d'agreger le contenu d'un dictionnaire à un jour qui est mis à jour
+print(dict1)
