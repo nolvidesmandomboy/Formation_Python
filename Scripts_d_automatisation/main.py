2527c49d4ae51505a24cbff9b85746a45a24b304
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-03-28 21:25:21 +0100

    Mise du "open" dans une variable, ouverture du fichier, écriture et fermeture du fichier. Sauvegarde automatique

--- Scripts_d_automatisation/main.py
+++ Scripts_d_automatisation/main.py
@@ -2,5 +2,11 @@
 
 #Sur Python, on peut ouvrir, éditer, sauvegarder et fermer un fichier.
 
-open("Mon_fichier.txt", "w")
-#ici, le open permet d'ouvrir le fichier et à l'intérieur des parenthèses on a le nom du ficher et le mode (ici "w" signifie write donc on va écrire mais ça aurait pu être autre chose. Vu que c'est w, si le fichier existe ça va supprimer le contenu et nous donner la possibilité d'écrire un nouveau truc, au contraire si ça n'existe pas ça crée le nouveau fichier pour nous permettre d'écrire.)
+f = open("Mon_fichier.txt", "w")
+'''ici, le open permet d'ouvrir le fichier et à l'intérieur des parenthèses on a le nom du ficher et le mode (ici "w" signifie write donc on va écrire mais ça aurait pu être autre chose. Vu que c'est w, si le fichier existe ça va supprimer le contenu et nous donner la possibilité d'écrire un nouveau truc, au contraire si ça n'existe pas ça crée le nouveau fichier pour nous permettre d'écrire.)
+
+Il faut faire aussi attention à ce que dans le terminal le chement soit le même que le fichier dans lequel on va éxécuter le script, c'est là qu'il va être enregistrer.
+'''
+f.write("Salut c'est Ninho ! X)")
+
+f.close()
