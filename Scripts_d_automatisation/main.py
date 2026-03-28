5312860843fc69509f8cd99a134494f5ad2dd4e9
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-03-28 21:44:39 +0100

    Commentaire du code précédent et début de l'exercice

--- Scripts_d_automatisation/main.py
+++ Scripts_d_automatisation/main.py
@@ -2,13 +2,16 @@
 
 #Sur Python, on peut ouvrir, éditer, sauvegarder et fermer un fichier.
 
-f = open("Mon_fichier.txt", "w")
+#f = open("Mon_fichier.txt", "w")
 '''ici, le open permet d'ouvrir le fichier et à l'intérieur des parenthèses on a le nom du ficher et le mode (ici "w" signifie write donc on va écrire mais ça aurait pu être autre chose. Vu que c'est w, si le fichier existe ça va supprimer le contenu et nous donner la possibilité d'écrire un nouveau truc, au contraire si ça n'existe pas ça crée le nouveau fichier pour nous permettre d'écrire.)
 
 Il faut faire aussi attention à ce que dans le terminal le chement soit le même que le fichier dans lequel on va éxécuter le script, c'est là qu'il va être enregistrer.
 '''
-f.write("Salut c'est Ninho ! X)")
+#f.write("Salut c'est Ninho ! X)")
 
-f.close()
+#f.close()
 
 #Le fichier write doit bien être mis avant le close, c'est pas possible de modifier le fichier une fois que c'est fermer.
+
+#Exercice 1
+
