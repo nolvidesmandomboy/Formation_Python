b4503cc295a88a2e9435a82613eef036fdce320f
nolvidesmandomboy <nolvidesmandomboy@gmail.com>
2026-03-28 21:19:51 +0100

    Utilisation de "Open" et écriture des commentaires pour des infos en plus

--- Scripts_d_automatisation/main.py
+++ Scripts_d_automatisation/main.py
@@ -1,2 +1,6 @@
 #Fichier Texte
-print("Hello word")
+
+#Sur Python, on peut ouvrir, éditer, sauvegarder et fermer un fichier.
+
+open("Mon_fichier.txt", "w")
+#ici, le open permet d'ouvrir le fichier et à l'intérieur des parenthèses on a le nom du ficher et le mode (ici "w" signifie write donc on va écrire mais ça aurait pu être autre chose. Vu que c'est w, si le fichier existe ça va supprimer le contenu et nous donner la possibilité d'écrire un nouveau truc, au contraire si ça n'existe pas ça crée le nouveau fichier pour nous permettre d'écrire.)
